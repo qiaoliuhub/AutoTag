f367d4568116240ec7611ac1754fc00d2feb608a
QiaoLiu <smileqiao1988@yahoo.com>
2017-09-08 23:39:25 -0400

    update model_generation.py

--- model_generation.py
+++ model_generation.py
@@ -1,9 +1,12 @@
 # Perform batch process to generate a classfication model
 # Extract TF-IDF features using spark and then train naive bayes classifier to do classification
+
 import logging
 import ConfigParser
+import pandas as pd
 
 from pyspark import SparkContext, SparkConf, SparkSession
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
 
 logging.basicConfig()
 logger=logging.getLogger('model_generation')
@@ -13,8 +16,10 @@ config=ConfigParser.ConfigParser()
 config.read('model_generation.cfg')
 
 master=config.get('spark','master')
-input_file=config.get('io', 'inputfile')
-tag_file=config.get('io', 'tagfile')
+posts_file=config.get('io', 'post_file')
+tags_file=config.get('io', 'tags_file')
+selected_tags_file=config.get('io', 'selected_tags_file')
+
 
 if __name__ == '__main__':
 
@@ -32,12 +37,33 @@ if __name__ == '__main__':
 	# Input the dataset
 	try:
 		logger.debug("Start to read the input dataset")
-		tags_df=spark.read.csv(input_file, header=True)
-		selected_tags=spark.read.csv(tag_file)
-		tag_set=set(selected_tags.select("_c0").rdd.flatMap(lambda x: int(x)).collect())
+		posts_df=spark.read.csv(posts_file, header=True)
+		tags_df=spark.read.csv(tags_file, header=True)
+		selected_tags=pd.read_csv(selected_tags_file, header=None)
+		tag_set=sc.broadcast(set(selected_tags[0]))
 		logger.debug("Read in dataset successfully")
 	except:
 		logger.debug("Can't input dataset")
+
+	# Join posts_df and tags_df together and prepare training dataset
+	selected_tags_df=tags_df.filter(tags_df['Tag'] in tag_set)
+	tags_questions_df=posts_df.join(selected_tags_df, posts_df.Id==selected_tags_df.Id)
+	training_df=tags_questions_df.select(['Tag', 'Body'])
+
+	# tokenize post texts and get term frequency and inverted document frequency
+	tokenizer=Tokenizer(inputCol="Body", outputCol="Words")
+	tokenized_words=tokenizer.transform(training_df)
+	hashing_TF=HashingTF(inputCol="Words", outputCol="Features")
+	TFfeatures=hashing_TF.transform(tokenized_words)
+
+	idf=IDF(inputCol="Features", outputCol="IDF_features")
+	idfModel=idf.fit(TFfeatures)
+	features=idfModel.transform(TFfeatures)
+
+	
+
+
+
 
 	
 
